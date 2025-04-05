38d338baf1194648ebd333a6a3addb67761705ef
Artem Shchedrov <art.worknsk@gmail.com>
2025-04-06 01:55:25 +0700

    добавление светлой темы, добавление package:provider/provider.dart

---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/themes/light_mode.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+}
