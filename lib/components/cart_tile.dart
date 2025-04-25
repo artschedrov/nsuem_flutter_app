3ccc0ea1f17b2c8cd32cfec701093fc3312ed3fe
Artem Shchedrov <art.worknsk@gmail.com>
2025-04-25 12:23:47 +0700

    страница заказа

--- lib/components/cart_tile.dart
+++ lib/components/cart_tile.dart
@@ -1,4 +1,6 @@
+import 'package:dart_extensions/dart_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:food_app/components/quntity_selector.dart';
 import 'package:food_app/models/cart_model.dart';
 import 'package:food_app/models/restaurant_model.dart';
 import 'package:provider/provider.dart';
@@ -11,28 +13,76 @@ class CartTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<RestaurantModel>(
         builder: (context, restaurant, child) => Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(8)
+          ),
+              margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
               child: Column(
                 children: [
-                  Row(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.asset(
-                          cartModel.food.imgPath,
-                          height: 100,
-                          width: 100,
-                        ),
+                  Padding(padding: const EdgeInsets.all(8.0),
+                   child: Row(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: [
+                       ClipRRect(
+                         borderRadius: BorderRadius.circular(8),
+                         child: Image.asset(
+                           cartModel.food.imgPath,
+                           height: 100,
+                           width: 100,
+                         ),
+                       ),
+                       const SizedBox(width: 10),
+                       Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: [
+                           Text(cartModel.food.name),
+                           Text(cartModel.food.price.toString(),
+                             style: TextStyle(
+                               color: Theme.of(context).colorScheme.primary
+                             ),
+                           ),
+                         ],
+                       ),
+                       Spacer(),
+                       QuantitySelector(
+                           quantity: cartModel.quantity,
+                           food: cartModel.food,
+                           onDecrement: () {
+                             restaurant.removeFromCart(cartModel);
+                           },
+                           onIncrement: (){
+                             restaurant.addToCart(cartModel.food, cartModel.selectedAddons);
+                           }
+                       )
+                     ],
+                   ),
+                  ),
+                  SizedBox(
+                    height: cartModel.selectedAddons.isEmpty ? 0 : 60,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: cartModel.selectedAddons
+                          .mapList(
+                            (addon) => Padding(
+                                padding: const EdgeInsets.only(right: 8.0),
+                              child: FilterChip(
+                                label: Row(
+                                  children: [
+                                    Text('${addon.name} '),
+                                    Text(addon.price.toString()),
+                                  ],
+                                ),
+                                shape: StadiumBorder(
+                                  side: BorderSide(
+                                    color: Theme.of(context).colorScheme.primary)),
+                                onSelected: (value) {},
+                                backgroundColor: Theme.of(context).colorScheme.secondary,
+                              ),
+                            )
+                          )
                       ),
-                      const SizedBox(width: 10),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(cartModel.food.name),
-                          Text(cartModel.food.price.toString()),
-                        ],
-                      )
-                    ],
-                  )
+                    ),
                 ],
               ),
             )
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/components/cart_tile.dart';
+import 'package:food_app/components/my_button.dart';
 import 'package:food_app/models/restaurant_model.dart';
 import 'package:provider/provider.dart';
 
@@ -16,22 +18,45 @@ class CartPage extends StatelessWidget {
             title: Text("Заказ"),
             backgroundColor: Colors.transparent,
             foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+            actions: [
+              IconButton(onPressed: () {
+                showDialog(context: context, builder: (context)=>AlertDialog(
+                  title: const Text("Вы хотите очистить корзину?"),
+                  actions: [
+                    TextButton(onPressed: () => Navigator.pop(context), child: const Text("Отмена")),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                          restaurant.clearCart();
+                        }, child: const Text("Да"))
+                  ],
+                ));
+                }, icon: const Icon(Icons.delete),
+              ) 
+            ],
           ),
           body: Column(
             children: [
               Expanded(
-                child: ListView.builder(
-                  itemCount: userCart.length,
-                  itemBuilder: (context, index) {
-                    final cartItem = userCart[index];
-                    return ListTile(
-                       title: Text(cartItem.food.name)
-                    );
-                  }
-                ),
+                  child: Column(
+                    children: [
+                      userCart.isEmpty ? const Expanded(child: Center(child: const Text("Корзина пустая.."),)) : Expanded(
+                        child: ListView.builder(
+                            itemCount: userCart.length,
+                            itemBuilder: (context, index) {
+                              final cartItem = userCart[index];
+                              return CartTile(cartModel: cartItem);
+
+                            }
+                        ),
+                      ),
+                    ],
+                  ),
               ),
+              MyButton(text: "Перейти к оплате", onTap: () {}),
+              const SizedBox(height: 25)
             ],
-          ),
+          )
         );
       }
     );
